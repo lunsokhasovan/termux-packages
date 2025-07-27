32ddb46c85776361e98271c248105d6759bff3a9
Sovan <lunsokhasovan@proton.me>
2025-07-27 15:03:06 +0700

    fix(main/kf6-kdoctools): misspelled depend

--- x11-packages/kf6-kdoctools/build.sh
+++ x11-packages/kf6-kdoctools/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSI
 TERMUX_PKG_SHA256=5c0c531a7978650f593a63a2aba1a641d590537e74c644aa361c63584b115aa2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="docbook-xsl, docbook-xml, libc++, libxml2, libxslt, kf6-karchive, kf6-ki18n, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="docxgen, extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="doxygen, extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
