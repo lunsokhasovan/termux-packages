30f0a2dacd8daf9a5465de681cb7e760f33d6532
Sovan <lunsokhasovan@proton.me>
2025-07-28 15:34:42 +0700

    Update build.sh

--- packages/liblangtag/build.sh
+++ packages/liblangtag/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.6.7
 TERMUX_PKG_SRCURL=https://bitbucket.org/tagoh/liblangtag/downloads/liblangtag-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=5ed6bcd4ae3f3c05c912e62f216cd1a44123846147f729a49fb5668da51e030e
-TERMUX_PKG_DEPENDS="libxml2"
+TERMUX_PKG_DEPENDS="gobject-introspection, libxml2"
 
 termux_step_pre_configure() {
 	export ac_cv_va_copy=C99
